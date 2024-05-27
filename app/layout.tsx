48ec6287abcdedb91922458c3afeb7f38c2e14da
Ritul Jain <rituljai@amazon.com>
2024-05-26 20:29:56 -0700

    Adding google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="f-N2DDVSssIWsJBvihqvgOtaBOSDsmCa1ZQq7jR4zXs" />
+      </head>
       <body className={afacad.variable + ' ' + cormorant_garamond.variable }>
         {children}
       </body>    
