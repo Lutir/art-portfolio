cc0db12ee1e1d05009955a761cd52c890213e1ce
Ritul <aloha@rituljain.com>
2024-08-11 19:06:01 -0700

    Added metatag description based on Next Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,39 @@
-"use client"
+import type { Metadata } from "next";
 import { afacad } from "@/app/ui/fonts";
 import { cormorant_garamond } from "@/app/ui/fonts";
+
 import '@/app/ui/globals.css';
-import Head from 'next/head';
-import { usePathname } from 'next/navigation';
+
+export const metadata: Metadata = {
+  title: "Ritul Jain Art Portfolio - Inspiring Acrylic Paintings of Nature",
+  description: "Explore the nature-inspired acrylic paintings of Ritul Jain, capturing the beauty of landscapes, mountains, and more. Discover art that evokes connection and inspiration.",
+  keywords: ['Acrylic Paintings', 'Nature Art', 'Landscape Paintings', 'Mountain Paintings', 'Ritul Jain', 'Art Portfolio', 'Inspiring Art'],
+  authors: [{ name: 'Ritul Jain' }],
+  metadataBase: new URL('https://art.rituljain.com'),
+  alternates: {
+    canonical: '/'
+  },
+  openGraph: {
+    title: 'Ritul Jain Art Portfolio - Inspiring Acrylic Paintings of Nature and Landscapes',
+    description: 'Discover the captivating acrylic paintings of Ritul Jain, featuring landscapes and natural beauty. Visit the gallery to see more.',
+    url: 'https://art.rituljain.com',
+    siteName: 'Ritul Jain Art Portfolio',
+    type: 'website'
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      'max-video-preview': 3,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  }
+};
 
 
 export default function RootLayout({
@@ -11,55 +41,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const pathname = usePathname();
-
-  // Determine the page and set the corresponding title and meta description
-  const seo = {
-    title: '',
-    description: '',
-    url: `https://art.rituljain.com${pathname}`,
-    // image: '/images/og-image.jpg', // Commented out for now
-  };
-
-  if (pathname === '/') {
-    seo.title = 'Ritul Jain Art Portfolio - Inspiring Acrylic Paintings of Nature';
-    seo.description = 'Explore the captivating acrylic paintings by Ritul Jain, featuring real-life landscapes, mountains, and nature. Discover art that inspires and connects with the beauty of the natural world.';
-  } else if (pathname === '/gallery') {
-    seo.title = 'Nature-Inspired Acrylic Art Gallery | Ritul Jain';
-    seo.description = 'Browse through a curated collection of acrylic paintings inspired by nature, capturing the essence of mountains, rivers, and more. Visit Ritul Jain\'s art gallery online.';
-  } else if (pathname === '/about') {
-    seo.title = 'About Ritul Jain - Artist Behind Nature\'s Beauty in Acrylic';
-    seo.description = 'Learn about Ritul Jain, the artist who brings the beauty of nature to life through acrylic paintings. Discover the inspiration and story behind each artwork.';
-  } else if (pathname === '/contact') {
-    seo.title = 'Contact Ritul Jain - Inquire About Acrylic Paintings';
-    seo.description = 'Get in touch with artist Ritul Jain for inquiries about his acrylic paintings, commissions, or collaborations. Connect with the artist behind the nature-inspired art.';
-  }
-
   return (
     <html lang="en">
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>{seo.title}</title>
-        <meta name="description" content={seo.description} />
-        <meta name="keywords" content="Acrylic Paintings, Nature Art, Landscape Paintings, Mountain Paintings, Ritul Jain, Art Portfolio" />
-
-        {/* Open Graph / Facebook */}
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={seo.title} />
-        <meta property="og:description" content={seo.description} />
-        <meta property="og:url" content={seo.url} />
-        {/* <meta property="og:image" content={seo.image} /> */}
-
-        {/* Twitter */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={seo.title} />
-        <meta name="twitter:description" content={seo.description} />
-        {/* <meta name="twitter:image" content={seo.image} /> */}
-
-        {/* Favicon */}
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-      </Head>
-      <body className={`${afacad.variable} ${cormorant_garamond.variable}`}>
+      <head>
+        <meta name="google-site-verification" content="f-N2DDVSssIWsJBvihqvgOtaBOSDsmCa1ZQq7jR4zXs" />
+        <link rel="icon" href="favicon.ico" sizes="any" />
+      </head>
+      <body className={afacad.variable + ' ' + cormorant_garamond.variable }>
         {children}
       </body>    
     </html>
